43f3642a99bd24015e27b7b56c8c68d3d7827080
Sodiq <sodiq7898@gmail.com>
2023-03-09 21:42:23 +0500

    added media

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -144,7 +144,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS=[
 #     BASE_DIR/"static"
 # ]
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL='/afterlogin'
