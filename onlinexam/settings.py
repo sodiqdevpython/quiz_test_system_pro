243d39957967d717dcd3e86a3f3e3916e9b37c19
Sodiq <sodiq7898@gmail.com>
2023-03-09 20:19:57 +0500

    media

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -144,6 +144,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS=[
     BASE_DIR/"static"
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
