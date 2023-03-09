28236dfc2ecaf4df096a7cf3c84edef058a3af83
Sodiq <sodiq7898@gmail.com>
2023-03-09 21:38:40 +0500

    st_root

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -136,13 +136,14 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS=[
-    BASE_DIR/"static"
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS=[
+#     BASE_DIR/"static"
+# ]
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
